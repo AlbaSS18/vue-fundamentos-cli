62a1196476f24092cad24513b93889dbf5e16954
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-11-02 21:02:02 +0100

    computed properties

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,12 +1,28 @@
 <template>
   <!-- Para crear el componente, se puede poner Vue, y por defecto se crea la estructura-->
-  <h2>Counter</h2>
-  <p></p>
+  <h2>Counter!!!</h2>
+  <p>{{counter}} <sup>2</sup> = {{ squareCounter }}</p>
+  <p>{{counter}} <sup>2</sup> = {{ squareCounter }}</p>
 </template>
 
 <script>
 export default {
-    name: 'Patito'
+    // name: 'Patito' // Se puede poner nombre a los componentes para identificarlos
+    data() {
+        return {
+            counter : 5
+        }
+    },
+    methods: {
+        getSquare(){
+            return this.counter * this.counter;
+        }
+    },
+    computed: {
+        squareCounter(){
+            return this.counter * this.counter;
+        }
+    }
 }
 </script>
 
